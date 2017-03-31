50b28c5cb30f24f262684b360374e41749671595
lbarias <licelotarias@gmail.com>
2017-03-31 11:52:24 -0600

    Add files via upload

---
+++ Demo/Demo/LogIn.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo
+{
+    /// <summary>
+    /// Interaction logic for LogIn.xaml
+    /// </summary>
+    public partial class LogIn : UserControl
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.switchToOwnerMainView();
+        }
+    }
+}
---
+++ Demo/Demo/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static MainWindow m;
+        public MainWindow()
+        {
+    
+            m = this; 
+            InitializeComponent();
+            LogIn login = new LogIn();
+            sp.Children.Add(login);
+
+        }
+
+        private void switchTo(UserControl u)
+        {
+            sp.Children.Clear();
+            sp.Children.Add(u);
+        }
+        public static void switchToLogInView()
+        {
+            LogIn lg = new LogIn();
+            m.switchTo(lg);
+        }
+
+        public static void switchToOwnerMainView()
+        {
+            Owner o = new Owner();
+            m.switchTo(o);
+        }
+    }
+}
